44c98f680c5ba477955dfdf7675ae0f8130cf3c9
D d <dd@Ds-MacBook-Air.local>
2025-01-11 15:36:11 +0800

    modify html table and add some features to .js

--- index.js
+++ index.js
@@ -1,35 +1,123 @@
 Papa.parse('data.csv', {
-    download: true,  // Automatically download the CSV file
-    header: true,    // First row contains headers
-    complete: function(results) {  // Called when parsing is complete
+    download: true,
+    header: true,
+    complete: function(results) {
         $('#gradeTable').DataTable({
-            data: results.data,    // The parsed CSV data
+            data: results.data,
             columns: [
-                { data: '課程名稱' },  // Maps column to 'StudentID' field in CSV
-                { data: '班次' },       // Maps column to 'Name' field
-                { data: '授課教師'},
-                { data: '學期'},
-                { data: 'A+比例'},
-                { data: 'A比例'},
-                { data: 'A-比例'},
-                { data: 'B+比例'},
-                { data: 'B比例'},
-                { data: 'B-比例'},
-                { data: 'C+比例'},
-                { data: 'C比例'},
-                { data: 'C-比例'},
-                { data: 'F比例'}
+                { 
+                    data: '課程名稱',
+                    title: '課程名稱'
+                },
+                { 
+                    data: '班次',
+                    title: '班次'
+                },
+                { 
+                    data: '授課教師',
+                    title: '授課教師'
+                },
+                { 
+                    data: '學期',
+                    title: '學期'
+                },
+                { 
+                    data: 'A+比例',
+                    title: 'A+',
+                    type: 'num',
+                    render: function(data) {
+                        return data ? data : '0%';
+                    }
+                },
+                { 
+                    data: 'A比例',
+                    title: 'A',
+                    type: 'num',
+                    render: function(data) {
+                        return data ? data : '0%';
+                    }
+                },
+                { 
+                    data: 'A-比例',
+                    title: 'A-',
+                    type: 'num',
+                    render: function(data) {
+                        return data ? data : '0%';
+                    }
+                },
+                { 
+                    data: 'B+比例',
+                    title: 'B+',
+                    type: 'num',
+                    render: function(data) {
+                        return data ? data : '0%';
+                    }
+                },
+                { 
+                    data: 'B比例',
+                    title: 'B',
+                    type: 'num',
+                    render: function(data) {
+                        return data ? data : '0%';
+                    }
+                },
+                { 
+                    data: 'B-比例',
+                    title: 'B-',
+                    type: 'num',
+                    render: function(data) {
+                        return data ? data : '0%';
+                    }
+                },
+                { 
+                    data: 'C+比例',
+                    title: 'C+',
+                    type: 'num',
+                    render: function(data) {
+                        return data ? data : '0%';
+                    }
+                },
+                { 
+                    data: 'C比例',
+                    title: 'C',
+                    type: 'num',
+                    render: function(data) {
+                        return data ? data : '0%';
+                    }
+                },
+                { 
+                    data: 'C-比例',
+                    title: 'C-',
+                    type: 'num',
+                    render: function(data) {
+                        return data ? data : '0%';
+                    }
+                },
+                { 
+                    data: 'F比例',
+                    title: 'F',
+                    type: 'num',
+                    render: function(data) {
+                        return data ? data : '0%';
+                    }
+                }
             ],
-            responsive: true,           // Makes table responsive on mobile
-            pageLength: 25,            // Shows 25 rows per page
-            order: [[2, 'asc']],       // Default sort by grade column (index 2) ascending
-            dom: 'Bfrtip',             // DataTables layout elements
-            buttons: [
-                'copy',                 // Adds button to copy data
-                'csv',                  // Adds button to export as CSV
-                'excel',                // Adds button to export as Excel
-                'pdf'                   // Adds button to export as PDF
-            ]
+            responsive: true,
+            pageLength: 25,
+            order: [[0, 'asc']],
+            dom: 'Bfrtip',
+            buttons: ['copy', 'csv', 'excel', 'pdf'],
+            language: {
+                search: "搜尋:",
+                lengthMenu: "顯示 _MENU_ 筆資料",
+                info: "顯示第 _START_ 至 _END_ 筆資料，共 _TOTAL_ 筆",
+                paginate: {
+                    first: "第一頁",
+                    previous: "上一頁",
+                    next: "下一頁",
+                    last: "最後一頁"
+                }
+            }
         });
     }
 });
